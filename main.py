5c9968bab256a5f98bb7ee93229b7cd53ed8b1b6
Roman-Zajic <zajic.roman.mobile@gmail.com>
2025-04-05 15:16:44 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,13 @@
+import pandas
+import xgboost as xgb
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+from sklearn.metrics import accuracy_score
+
+def print_hi(name):
+    print(f'Hi, {name}')
+
+
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
